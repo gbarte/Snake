a76dbb833f9ad960050944a61c8336f391e9022a
Mirijam <s.x.zhang@student.tudelft.nl>
2019-11-27 15:06:46 +0100

    Created GameScreen with snake

---
+++ src/main/java/SnakeGameScreen.java
@@ -0,0 +1,131 @@
+import com.badlogic.gdx.*;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+
+public class SnakeGameScreen implements Screen {
+    private OrthographicCamera camera;
+    private FitViewport viewport;
+    private SpriteBatch batch;
+    private Sprite sprite;
+    private SnakeBody snake;
+    private STATE currState;
+    private ShapeRenderer shapeRenderer;
+
+    public enum STATE {
+        GAME_READY, GAME_PAUSED, GAME_PLAYING, GAME_OVER;
+    }
+
+    public OrthographicCamera getCamera() {
+        return camera;
+    }
+
+    public void setCamera(OrthographicCamera camera) {
+        this.camera = camera;
+    }
+
+    public FitViewport getViewport() {
+        return viewport;
+    }
+
+    public void setViewport(FitViewport viewport) {
+        this.viewport = viewport;
+    }
+
+    public SpriteBatch getBatch() {
+        return batch;
+    }
+
+    public void setBatch(SpriteBatch batch) {
+        this.batch = batch;
+    }
+
+    public Sprite getSprite() {
+        return sprite;
+    }
+
+    public void setSprite(Sprite sprite) {
+        this.sprite = sprite;
+    }
+
+    public SnakeBody getSnake() {
+        return snake;
+    }
+
+    public void setSnake(SnakeBody snake) {
+        this.snake = snake;
+    }
+
+    public STATE getCurrState() {
+        return currState;
+    }
+
+    public void setCurrState(STATE currState) {
+        this.currState = currState;
+    }
+
+    public ShapeRenderer getShapeRenderer() {
+        return shapeRenderer;
+    }
+
+    public void setShapeRenderer(ShapeRenderer shapeRenderer) {
+        this.shapeRenderer = shapeRenderer;
+    }
+
+    @Override
+    public void show() {
+        currState = STATE.GAME_READY;
+        shapeRenderer = new ShapeRenderer();
+        snake = new SnakeBody();
+        camera = new OrthographicCamera(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        viewport = new FitViewport(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        batch = new SpriteBatch();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        viewport.update(width, height);
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+//        camera.update();
+        Gdx.gl.glClearColor(0, 0, 1, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        batch.begin();
+        snake.renderSnake(camera, shapeRenderer);
+        batch.end();
+
+    }
+
+    @Override
+    public void dispose() {
+        batch.dispose();
+    }
+
+    private void queryInput() {
+
+    }
+
+}
+
+
