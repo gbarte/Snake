23f1db525fcb6b08ba35d5d9fa8b8b9b8c687ab0
Mirijam <s.x.zhang@student.tudelft.nl>
2019-11-27 16:23:56 +0100

    Added JavaDoc

--- src/main/java/BodyPart.java
+++ src/main/java/BodyPart.java
@@ -5,6 +5,7 @@ import com.badlogic.gdx.graphics.g3d.utils.shapebuilders.BoxShapeBuilder;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.Shape;
+import org.lwjgl.util.Rectangle;
 import org.w3c.dom.Text;
 
 import java.util.LinkedList;
@@ -12,21 +13,13 @@ import java.util.LinkedList;
 public class BodyPart {
     private float headX, headY;
     private float x, y;
-    ShapeRenderer shapeRenderer;
-    ShapeRenderer.ShapeType bp;
     //add body part image or rectangle
 //    Texture texture;
 
-    //have to add width height according to mapsize
-//    public BodyPart(Texture texture, int headX, int headY) {
     public BodyPart(int headX, int headY) {
 //        this.texture = texture;
         this.headX = headX;
         this.headY = headY;
-//        shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
-//        shapeRenderer.setColor(Color.GREEN);
-//        shapeRenderer.rect(headX, headY, 1, 1);
-//        shapeRenderer.end();
     }
 
     public float getHeadX() {
@@ -60,31 +53,5 @@ public class BodyPart {
     public void setY(float y) {
         this.y = y;
     }
-
-    public ShapeRenderer getShapeRenderer() {
-        return shapeRenderer;
-    }
-
-    public void setShapeRenderer(ShapeRenderer shapeRenderer) {
-        this.shapeRenderer = shapeRenderer;
-    }
-
-    public ShapeRenderer.ShapeType getBp() {
-        return bp;
-    }
-
-    public void setBp(ShapeRenderer.ShapeType bp) {
-        this.bp = bp;
-    }
-
-//    public void drawBody() {
-//        if(x != headX || y != headY) {
-////            batch.draw(texture, x, y);
-//            shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
-//            shapeRenderer.setColor(Color.GREEN);
-//            shapeRenderer.rect(headX, headY, 1, 1);
-//            shapeRenderer.end();
-//        }
-//    }
 
 }
