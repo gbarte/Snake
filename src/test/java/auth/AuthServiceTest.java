1fe0151fd032b57091f7e9dbc2eb617785210dd8
rsirokov <r.sirokov@student.tudelft.nl>
2019-12-05 15:48:40 +0100

    Add basic implementation of authentication

--- src/test/java/auth/AuthServiceTest.java
+++ src/test/java/auth/AuthServiceTest.java
@@ -1,4 +1,57 @@
 package auth;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class AuthServiceTest {
+
+    private AuthService service;
+
+    @BeforeEach
+    void setUp() {
+
+        service = new AuthService("test_table");
+
+        RegistrationResponse response = service.register("john", "12345678");
+    }
+
+    @Test
+    void authFailureTest() {
+        AuthResponse response = service.auth("john", "123456");
+        Assertions.assertEquals(AuthResponse.WRONG_PASSWORD, response);
+    }
+
+    @Test
+    void registrationShortFailureTest() {
+        RegistrationResponse response = service.register("john", "123");
+        Assertions.assertEquals(RegistrationResponse.SHORT_PASSWORD, response);
+    }
+
+    @Test
+    void registrationSuccessTest() {
+        RegistrationResponse response = service.register("sem10", "ROMAN_IS_BEST");
+        Assertions.assertEquals(RegistrationResponse.SUCCESS, response);
+    }
+
+    @Test
+    void authenticationSuccessTest() {
+        AuthResponse authResponse = service.auth("john", "12345678");
+        Assertions.assertEquals(AuthResponse.SUCCESS, authResponse);
+    }
+
+    @Test
+    void authenticationFailureTest() {
+        AuthResponse authResponse = service.auth("john", "123");
+        Assertions.assertEquals(AuthResponse.WRONG_PASSWORD, authResponse);
+    }
+
+    @Test
+    void nonExistentUserTest() {
+        AuthResponse authResponse = service.auth("i_dont_exist", "12345678");
+        Assertions.assertEquals(AuthResponse.WRONG_PASSWORD, authResponse);
+    }
+
 }
