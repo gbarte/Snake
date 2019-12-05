ae4afadce108a592ca1cf3f18aab729b3b3cc14b
Mirijam <s.x.zhang@student.tudelft.nl>
2019-12-05 12:02:27 +0100

    Created Loginscreen

--- src/main/java/states/LoginState.java
+++ src/main/java/states/LoginState.java
@@ -1,62 +1,94 @@
 package states;
 
-import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.*;
-import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
+import com.badlogic.gdx.utils.Align;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
-import org.w3c.dom.Text;
 
-public class LoginState extends ApplicationAdapter {
-    Stage stage;
-//    private static final int BUTTON_WIDTH = 100;
-//    private static final int BUTTON_HEIGHT = 30;
-    private ImageButton loginButton;
-    private ImageButton signUpButton;
-//    private Texture loginButtonActive;
-//    private Texture signUpButtonActive;
-//    private TextField usernameField;
-//    private TextField passWordField;
-//    private Texture snake;
-//    private Texture backGround;
-//    private SpriteBatch batch;
-
-//    public LoginState(GameStateManager gameManager) {
-//        super(gameManager);
-//        loginButton = new Texture("login_button");
-//        signUpButton = new Texture("sign_up_button");
-//    }
-//
-//    @Override
-//    public void handleInput() {
-//
-//    }
-//
-//    @Override
-//    public void update(float dt) {
-//
-//    }
-
-    public void create() {
+public class LoginState extends State {
+    private Stage stage;
+    private Skin skin;
+    private Label title;
+    private Button loginButton;
+    private Button signUpButton;
+    private TextField usernameField;
+    private TextField passWordField;
+    private Texture backGround;
+    /**
+     * Constructor which creates a new state within the game.
+     * E.g. Play/Pause/Menu.
+     *
+     * @param gameManager which keeps track of the state of the game.
+     */
+    public LoginState(GameStateManager gameManager) {
+        super(gameManager);
         stage = new Stage(new ScreenViewport());
         Gdx.input.setInputProcessor(stage);
-        TextureRegionDrawable loginUp = new TextureRegionDrawable(new TextureRegion(new Texture(Gdx.files.internal("assets/login_button2.png"))));
-        TextureRegionDrawable loginDown = new TextureRegionDrawable(new TextureRegion(new Texture(Gdx.files.internal("assets/login_button.png"))));
-        loginButton = new ImageButton(loginUp, loginDown);
-        loginButton.setPosition(650, 350);
+        skin =  new Skin(Gdx.files.internal("assets/quantum-horizon/skin/quantum-horizon-ui.json"));
+        initTitle();
+        initLogin();
+        initSignUp();
+        backGround = new Texture("assets/login_screen2.png");
+    }
+
+    private void initTitle() {
+//        BitmapFont bitmapFont = new BitmapFont();
+//        Label.LabelStyle labelStyle = new Label.LabelStyle(bitmapFont, Color.GREEN);
+//        title = new Label("Schitzosnake", labelStyle);
+        title = new Label("Shitzosnake", skin);
+        title.setSize(600, 120);
+        title.setPosition(100 ,450);
+        title.setFontScale(3);
+        title.setAlignment(Align.center);
+        stage.addActor(title);
+    }
+
+    private void initSignUp() {
+        TextureRegionDrawable signUp = new TextureRegionDrawable(new TextureRegion(new Texture(Gdx.files.internal("assets/sign_up_button2.png"))));
+        TextureRegionDrawable signDown = new TextureRegionDrawable(new TextureRegion(new Texture(Gdx.files.internal("assets/sign_up_button.png"))));
+        signUpButton = new ImageButton(signUp, signDown);
+//        signUpButton = new TextButton("Sign Up", new Skin(Gdx.files.internal("assets/neon/skin/neon-ui.json")));
+        signUpButton.setPosition(300, 150);
+        signUpButton.addListener(new InputListener(){
+            @Override
+            public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
+                gameManager.set(new SignUpState(gameManager));
+            }
+            @Override
+            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+                // TODO
+                System.out.println("pressed");
+                return true;
+            }
+        });
+        passWordField = new TextField("", new Skin(Gdx.files.internal("assets/cloud-form/skin/cloud-form-ui.json")));
+        passWordField.setSize(150, 30);
+        passWordField.setPosition(100, 150);
+        passWordField.isPasswordMode();
+        passWordField.setPasswordCharacter('*');
+        stage.addActor(signUpButton);
+        stage.addActor(passWordField);
+
+    }
+
+    private void initLogin() {
+        loginButton = new TextButton("Login", skin);
+        loginButton.setPosition(300, 200);
         loginButton.addListener(new InputListener(){
             @Override
             public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
-                System.out.println("press");
+//                gameManager.set(new MenuState(gameManager));
+                gameManager.set(new PlayState(gameManager));
             }
             @Override
             public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
@@ -64,74 +96,33 @@ public class LoginState extends ApplicationAdapter {
                 return true;
             }
         });
-        TextureRegionDrawable signUp = new TextureRegionDrawable(new TextureRegion(new Texture(Gdx.files.internal("assets/sign_up_button2.png"))));
-        TextureRegionDrawable signDown = new TextureRegionDrawable(new TextureRegion(new Texture(Gdx.files.internal("assets/sign_up_button.png"))));
-        signUpButton = new ImageButton(signUp, signDown);
-        signUpButton.setPosition(650, 300);
-        signUpButton.addListener(new InputListener(){
-            @Override
-            public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
-                System.out.println("press");
-            }
-            @Override
-            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
-                // TODO
-//                System.out.println("pressed");
-                return true;
-            }
-        });
+        usernameField = new TextField("", skin);
+        usernameField.setSize(150, 30);
+        usernameField.setPosition(100, 200);
         stage.addActor(loginButton);
-        stage.addActor(signUpButton);
-//        batch = new SpriteBatch();
-//        Skin skin = new Skin(Gdx.files.internal("assets/neon/skin/neon-ui.json"));
-//        loginButton = new Texture("assets/login_button2.png");
-//        loginButtonActive = new Texture("assets/login_button.png");
-//        signUpButton = new Texture("assets/sign_up_button2.png");
-//        signUpButtonActive = new Texture("assets/sign_up_button.png");
-//        usernameField = new TextField("username", skin);
-//        passWordField = new TextField("password", skin);
-//        snake = new Texture("assets/snake-png-vector.png");
-//        backGround = new Texture("assets/login_screen2.png");
-//        backgroundSprite = new Sprite(backGround);
+        stage.addActor(usernameField);
     }
 
     @Override
-//    public void render(SpriteBatch batch) {
-    public void render() {
+    public void handleInput() {
+
+    }
+
+    @Override
+    public void update(float dt) {
+
+    }
+
+    @Override
+    public void render(SpriteBatch batch) {
         Gdx.gl.glClearColor((float) 0.61, (float) 0.77, (float) 0.65, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         stage.act();
+        stage.getBatch().begin();
+        stage.getBatch().draw(backGround, 0, 0, 800, 800);
+        stage.getBatch().end();
         stage.draw();
-//        batch.begin();
-////        backgroundSprite.draw(batch);
-//
-//        batch.draw(backGround, 0, 0, 800, 800);
-//        batch.draw(snake, 0, 0, 350, 350);
-//
-//        batch.draw(loginButton,  650, 350);
-//        batch.draw(signUpButton, 650, 300);
-//
-//        inputHandler();
-////        System.out.println(yCoordinate);
-//
-//
-//        batch.end();
     }
-
-//    public void inputHandler() {
-//        float xCoordinate = Gdx.input.getX();
-//        float yCoordinate = Gdx.input.getY();
-//        if (xCoordinate > 650 && xCoordinate < 650 + BUTTON_WIDTH  && yCoordinate < 350 + BUTTON_HEIGHT && yCoordinate > 350) {
-//            batch.draw(loginButtonActive, 650, 350);
-//        } else {
-//            batch.draw(loginButton, 650, 350);
-//        }
-//        if (xCoordinate > 650 && xCoordinate < 650 + BUTTON_WIDTH  && yCoordinate < 300 + BUTTON_HEIGHT && yCoordinate > 300) {
-//            batch.draw(signUpButtonActive, 650, 300);
-//        } else {
-//            batch.draw(signUpButton, 650, 300);
-//        }
-//    }
 
     @Override
     public void dispose() {
