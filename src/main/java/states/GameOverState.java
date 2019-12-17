af7b960ba291501bc170634d2726beae3747cccf
Gabriele Barteskaite <g.barteskaite@student.tudelft.nl>
2019-12-17 22:54:11 +0100

    Create GameOverState class

---
+++ src/main/java/states/GameOverState.java
@@ -0,0 +1,48 @@
+package states;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+
+public class GameOverState extends State {
+    private Stage stage;
+//    private Skin skin;
+    private Texture backGround;
+    /**
+     * Constructor which creates a new state within the game.
+     * E.g. Play/Pause/Menu.
+     *
+     * @param gameManager which keeps track of the state of the game.
+     */
+    public GameOverState(GameStateManager gameManager) {
+        super(gameManager);
+        stage = new Stage(new ScreenViewport());
+        Gdx.input.setInputProcessor(stage);
+//        skin =  new Skin();
+//        backGround = new Texture("assets/...");
+    }
+
+    @Override
+    public void handleInput() {
+
+    }
+
+    @Override
+    public void update(float dt) {
+
+    }
+
+    @Override
+    public void render(SpriteBatch batch) {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
