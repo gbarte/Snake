f0ccfb84b58be6e80e9b595c01d42b6fb1fb4d4f
Gabriele Barteskaite <g.barteskaite@student.tudelft.nl>
2019-12-18 11:30:55 +0100

    Add title to GameOverState

--- src/main/java/states/GameOverState.java
+++ src/main/java/states/GameOverState.java
@@ -1,16 +1,20 @@
 package states;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.utils.Align;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 
 public class GameOverState extends State {
     private Stage stage;
 //    private Skin skin;
+    private Label title;
     private Texture backGround;
     /**
      * Constructor which creates a new state within the game.
@@ -23,7 +27,31 @@ public class GameOverState extends State {
         stage = new Stage(new ScreenViewport());
         Gdx.input.setInputProcessor(stage);
 //        skin =  new Skin();
+        initTitle();
         backGround = new Texture("assets/glitchy.png");
+    }
+
+    /**
+     * Sets title of Game Over screen.
+     */
+    private void initTitle() {
+        BitmapFont bitmapFont = new BitmapFont(Gdx.files.internal("assets/font.fnt"));
+        Label.LabelStyle labelStyle = new Label.LabelStyle(bitmapFont,
+                new Color((float) 0.9,0, 0, 1));
+        title = new Label("Game Over", labelStyle);
+        title.setSize(600, 120);
+        title.setPosition(10,400);
+        title.setFontScale(3);
+        title.setAlignment(Align.center);
+        stage.addActor(title);
+    }
+
+    public Texture getBackGround() {
+        return backGround;
+    }
+
+    public void setBackGround(Texture backGround) {
+        this.backGround = backGround;
     }
 
     @Override
