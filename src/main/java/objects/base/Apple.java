fd6d567be0b7c449eb28726ed93b060f4ca76046
Sanjay Rachwani <S.Rachwani@student.tudelft.nl>
2019-12-22 03:29:16 +0100

    Preparing for integration with existing classes

---
+++ src/main/java/objects/base/Apple.java
@@ -0,0 +1,97 @@
+package objects.base;
+
+import com.badlogic.gdx.graphics.Texture;
+import gamelogic.Coordinate;
+
+import java.util.Random;
+import snake.SnakeBody;
+import utils.Sizes;
+
+/**
+ * Interactive food object of Apple.
+ */
+public class Apple {
+
+    private static int DEFAULT_SCORE = 10;
+
+    private Coordinate coordinate;
+    private int score;
+    private Texture texture;
+    private static final String texturePath = "assets/greenapplesmall.png";
+
+    /**
+     * Creates an apple with a predefined texture at given coordinate in the
+     * texture space (Coordinate is multiplied with cell size).
+     * @param x coordinate of the apple on the map.
+     * @param y coordinate of the apple on the map.
+     */
+    public Apple(int x, int y) {
+        this.coordinate = new Coordinate(x * SnakeBody.CELL_SIZE, y * SnakeBody.CELL_SIZE);
+        this.score = DEFAULT_SCORE;
+        this.texture = new Texture(texturePath);
+    }
+
+    /**
+     * Creates an apple with a predefined texture at Random coordinate in the
+     * texture space (Coordinate is multiplied with cell size!).
+     */
+    public Apple() {
+        Random r = new Random();
+        int minX = 0;
+        int minY = 0;
+        int maxX = Sizes.DEFAULT_MINIMUM_MAP_TILES;
+        int maxY = Sizes.DEFAULT_MINIMUM_MAP_TILES;
+
+        int x = r.nextInt(maxX - minX) + minX;
+        int y = r.nextInt(maxY - minY) + minY;
+
+        Coordinate coord = new Coordinate(x * SnakeBody.CELL_SIZE, y * SnakeBody.CELL_SIZE);
+        this.coordinate = coord;
+        this.score = 10;
+        this.texture = new Texture(texturePath);
+    }
+
+    /**
+     * Creates an apple with given texture and coordinate, as well as the score
+     * that will get the player when eating this apple. This method was mainly created
+     * for testing as you pass your own (mocked) texture.
+     * @param x coordinate of the apple on the map.
+     * @param y coordinate of the apple on the map.
+     * @param score that eating this apple gives to the player.
+     * @param texture of the apple on the map (has to be of the same size as cell).
+     */
+    public Apple(int x, int y, int score, Texture texture) {
+        this.coordinate = new Coordinate(x * SnakeBody.CELL_SIZE, y * SnakeBody.CELL_SIZE);
+        this.score = score;
+        this.texture = texture;
+    }
+
+    public Apple(int x, int y, int score) {
+        this.coordinate = new Coordinate(x * SnakeBody.CELL_SIZE, y * SnakeBody.CELL_SIZE);
+        this.score = score;
+    }
+
+    public Coordinate getCoordinate() {
+        return coordinate;
+    }
+
+    public void setCoordinate(Coordinate coordinate) {
+        this.coordinate = coordinate;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public Texture getTexture() {
+        return texture;
+    }
+
+    public void setTexture(Texture texture) {
+        this.texture = texture;
+    }
+}
---
+++ src/main/java/snake/SnakeBody.java
@@ -0,0 +1,151 @@
+package snake;
+
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import gamelogic.Coordinate;
+import utils.Sizes;
+
+import java.util.LinkedList;
+
+/**
+ * Class that defines the snake's body logics.
+ */
+public class SnakeBody {
+    public static final int CELL_SIZE = Sizes.TILE_PIXELS;
+    private static final int INITIAL_LENGTH = 2;
+
+    private Coordinate headCoord;
+    private LinkedList<BodyPart> bodyParts;
+    private Direction currDir;
+
+    public enum Direction { LEFT, RIGHT, UP, DOWN }
+
+    /**
+     * Constructs a snake with INITIAL_LENGTH amount of bodyparts.
+     *
+     * @param headX - X coordinate of head
+     * @param headY - Y coordinate of head
+     */
+    public SnakeBody(int headX, int headY) {
+        this.headCoord = new Coordinate(headX / 2, headY / 2);
+        this.currDir = Direction.RIGHT;
+        this.bodyParts = new LinkedList<BodyPart>();
+        growSnake(INITIAL_LENGTH);
+    }
+
+    public LinkedList<BodyPart> getBodyParts() {
+        return bodyParts;
+    }
+
+    public void setBodyParts(LinkedList<BodyPart> bodyParts) {
+        this.bodyParts = bodyParts;
+    }
+
+    public Direction getCurrDir() {
+        return currDir;
+    }
+
+    public void setCurrDir(Direction currDir) {
+        this.currDir = currDir;
+    }
+
+    public Coordinate getHeadCoord() {
+        return headCoord;
+    }
+
+    public void setHeadCoord(Coordinate headCoord) {
+        this.headCoord = headCoord;
+    }
+
+    /**
+     * Grows the snake body by one body part.
+     */
+    public void growSnake() {
+        if (bodyParts.size() == 0) {
+            bodyParts.add(new BodyPart(headCoord.getCoordinateX(), headCoord.getCoordinateY()));
+        } else if (bodyParts.size() > 0) {
+            int tailID = bodyParts.size() - 1;
+            BodyPart tail = bodyParts.get(tailID);
+            Coordinate tailCoord = tail.getCoordinate();
+            bodyParts.add(new BodyPart(tailCoord.getCoordinateX(), tailCoord.getCoordinateY()));
+        }
+    }
+
+    /**
+     * Grows the snake body by a specified number of body parts.
+     *
+     * @param length - by how many body parts the snake will be grown
+     */
+    public void growSnake(int length) {
+        for (int i = 0; i < length; i++) {
+            this.growSnake();
+        }
+    }
+
+    /**
+     * First renders the head of the snake as a rectangle.
+     * Then loops through the bodyparts and renders those.
+     *
+     * @param shapeRenderer - ShapeRenderer object
+     */
+    @SuppressWarnings("PMD.DataflowAnomalyAnalysis")
+    public void renderSnake(ShapeRenderer shapeRenderer) {
+        shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
+        shapeRenderer.setColor(new Color(Color.GREEN));
+        int x = this.headCoord.getCoordinateX();
+        int y = this.headCoord.getCoordinateY();
+        shapeRenderer.rect(x, y, CELL_SIZE, CELL_SIZE);
+        if (bodyParts.size() > 0) {
+            for (BodyPart bp : bodyParts) {
+                Coordinate bodyCoord = bp.getCoordinate();
+                shapeRenderer.rect(bodyCoord.getCoordinateX(), bodyCoord.getCoordinateY(),
+                        CELL_SIZE, CELL_SIZE);
+            }
+        }
+        shapeRenderer.end();
+    }
+
+    /**
+     * Updates currDir to the given direction.
+     *
+     * @param snakeDirection - Updates currDir to this direction
+     */
+    public void moveSnake(Direction snakeDirection) {
+        switch (snakeDirection) {
+            case RIGHT:
+                updateBodyPartsPosition(headCoord);
+                headCoord.addToX(CELL_SIZE);
+                break;
+            case LEFT:
+                updateBodyPartsPosition(headCoord);
+                headCoord.subtractFromX(CELL_SIZE);
+                break;
+            case UP:
+                updateBodyPartsPosition(headCoord);
+                headCoord.addToY(CELL_SIZE);
+                break;
+            case DOWN:
+                updateBodyPartsPosition(headCoord);
+                headCoord.subtractFromY(CELL_SIZE);
+                break;
+            default:
+                // will not execute
+        }
+    }
+
+    /**
+     * Updates the position of each body part.
+     */
+    @SuppressWarnings("PMD.DataflowAnomalyAnalysis")
+    public void updateBodyPartsPosition(Coordinate coordinate) {
+        if (bodyParts.size() > 0) {
+            for (BodyPart bp : bodyParts) {
+                int currX = bp.getCoordinate().getCoordinateX();
+                int currY = bp.getCoordinate().getCoordinateY();
+                bp.updateBodyPartPos(coordinate);
+                coordinate = new Coordinate(currX, currY);
+            }
+        }
+    }
+
+}
