80c94133c94f24cdbdcb7ccbf2a707d09e0fa561
Sanjay Rachwani <S.Rachwani@student.tudelft.nl>
2020-01-09 15:12:47 +0100

    Apple doesn't generate on the border.

--- src/main/java/objects/base/Apple.java
+++ src/main/java/objects/base/Apple.java
@@ -37,15 +37,16 @@ public class Apple {
      */
     public Apple() {
         Random r = new Random();
-        int minX = 0;
-        int minY = 0;
-        int maxX = Sizes.DEFAULT_MINIMUM_MAP_TILES;
-        int maxY = Sizes.DEFAULT_MINIMUM_MAP_TILES;
+        int minX = Sizes.DEFAULT_AMOUNT_BORDER_TILES;
+        int minY = Sizes.DEFAULT_AMOUNT_BORDER_TILES;
+        int maxX = Sizes.DEFAULT_MINIMUM_MAP_TILES - Sizes.DEFAULT_AMOUNT_BORDER_TILES;
+        int maxY = Sizes.DEFAULT_MINIMUM_MAP_TILES - Sizes.DEFAULT_AMOUNT_BORDER_TILES;
 
         int x = r.nextInt(maxX - minX) + minX;
         int y = r.nextInt(maxY - minY) + minY;
 
         Coordinate coord = new Coordinate(x, y);
+        
         this.coordinate = coord;
         this.score = DEFAULT_SCORE;
         this.texture = new Texture(texturePath);
