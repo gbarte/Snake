27bf2556cb23be0c30440f349fdac6380c90c914
Mirijam Zhang <s.x.zhang@student.tudelft.nl>
2020-01-10 19:29:14 +0000

    Update SnakeGame.java

--- src/main/java/game/SnakeGame.java
+++ src/main/java/game/SnakeGame.java
@@ -25,7 +25,7 @@ public class SnakeGame extends ApplicationAdapter {
         batch = new SpriteBatch();
         gameManager = new GameStateManager();
         Gdx.gl.glClearColor(0, 0, 0, 1);
-        gameManager.push(new PlayState(gameManager));
+        gameManager.push(new LoginState(gameManager));
     }
 
     @Override
